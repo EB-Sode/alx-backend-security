3220d296752b30c0b727a136b840a6df79de2c51
Blessed Sode <emi.square23@gmail.com>
2025-11-08 18:02:22 +0100

    hardcoded allowed host temporarily

--- ip_tracking/settings.py
+++ ip_tracking/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['ip-tracking-w1wa.onrender.com', 'localhost', '127.0.0.1']
+
+
 
 
 
