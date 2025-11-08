0147b82f97a9c0208d35e5468114ccc6cf655fd9
Blessed Sode <emi.square23@gmail.com>
2025-11-08 18:55:23 +0100

    fixed error

--- ip_tracking/settings.py
+++ ip_tracking/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key-for-dev')
 
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS = ['ip-tracking-w1wa.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = ['ip-tracking-w1wa.onrender.com', 'localhost', '127.0.0.1']
 
 
 
