1014b5e343444d63b9690be7e5e437c7f21609d7
Blessed Sode <emi.square23@gmail.com>
2025-11-08 21:05:47 +0100

    fixed shortcuts

--- ip_tracking/urls.py
+++ ip_tracking/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django_shortcuts import redirect
+from django.shortcuts import redirect
 
 schema_view = get_schema_view(
     openapi.Info(
